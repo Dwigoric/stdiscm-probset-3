abd7b4e8083112754ac2831fe132532b00a165b3
Joshua Permito <git.commits@dwigoric.dev>
2025-03-31 16:54:33 +0800

    chore: set up JavaFX project

---
+++ JavaFX/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module ph.dlsu.edu.ccs.stdiscm.jgang.probset3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens ph.dlsu.edu.ccs.stdiscm.jgang.probset3 to javafx.fxml;
+    exports ph.dlsu.edu.ccs.stdiscm.jgang.probset3;
+}
---
+++ JavaFX/src/main/java/ph/dlsu/edu/ccs/stdiscm/jgang/probset3/HelloController.java
@@ -0,0 +1,14 @@
+package ph.dlsu.edu.ccs.stdiscm.jgang.probset3;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
