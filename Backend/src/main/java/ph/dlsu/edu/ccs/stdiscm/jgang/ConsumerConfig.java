7931969ca70c8f9dea1677fdd1741d829bb12e5f
Joshua Permito <git.commits@dwigoric.dev>
2025-04-05 23:35:14 +0800

    Revert "fix: use higher level directories for videos"
    This reverts commit cc795852ee2899787a46b52d2f94d9049e25eab2.

--- Backend/src/main/java/ph/dlsu/edu/ccs/stdiscm/jgang/ConsumerConfig.java
+++ Backend/src/main/java/ph/dlsu/edu/ccs/stdiscm/jgang/ConsumerConfig.java
@@ -4,6 +4,6 @@ public class ConsumerConfig {
     public static final String SERVER_IP = "127.0.0.1";
     public static final int SERVER_PORT = 3005;
     public static final int NUM_THREADS = 3;
-    public static final String DOWNLOAD_FOLDER = "../download_videos/"; // Storage Location
+    public static  final String DOWNLOAD_FOLDER = "download_videos/"; // Storage Location
     public static final int MAX_QUEUE_SIZE = 5; // Leaky bucket queue size
 }
